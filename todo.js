3213fa864b15009f75b4980cd205427dc1ee09d5
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-02 23:04:41 +0300

    Saving tasks in local storage

--- todo.js
+++ todo.js
@@ -6,7 +6,7 @@ var task = $('.task');
 var add = $("#addButton");
 $(add).click(function(){
     for (var i = 0; i < task.length; i++) {
-        console.log(task[i].value);
+        console.log(task[i].value);}
     
 $("#incomplete").val(
     $("#incomplete").val() +
@@ -14,7 +14,22 @@ $("#incomplete").val(
      " , description: "+$("#desc").val() +
      ", point: "+$("#point").val()
     );
-}})
+})
+ localStorage.setItem("Task:", task)
+
+ 
+// var edit =$("#editButton");
+// var isDone =$("#isDone");
+// var remove=$("#deleteButton");
+
+
+// $(edit).click(function(){
+//     $('li').attr('contenteditable','true');
+//  });
+ 
+//  $(remove).click(function(){
+//     $('incomplete').remove();
+//  });
 
 
 
