da02c6e7dd440b2180195be2b63586535cc01d10
Nour Mshawrab <mshawrabnour@gmail.com>
2022-06-02 22:45:17 +0300

    Adding task to incomplete category

--- todo.js
+++ todo.js
@@ -7,8 +7,14 @@ var add = $("#addButton");
 $(add).click(function(){
     for (var i = 0; i < task.length; i++) {
         console.log(task[i].value);
-    }})
-
+    
+$("#incomplete").val(
+    $("#incomplete").val() +
+    "Title: "+$("#title").val() + 
+     " , description: "+$("#desc").val() +
+     ", point: "+$("#point").val()
+    );
+}})
 
 
 
